472d0b3612d1df32e3a5c56e0e7c27d9b76b97ab
Kaplin Dima <Korvin777KDS@mail.ru>
2021-08-01 20:45:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw_design_onne/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hw_design_onne
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hw_design_onne.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
